1b07f0d4286b767818323d566a95eace96bfecb8
nairobi <glimglam@protonmail.com>
2026-05-18 19:50:20 +0200

    fix: proper ID check

--- typescript/seeds-scripts/rerunners/fix-folder-ids.js
+++ typescript/seeds-scripts/rerunners/fix-folder-ids.js
@@ -5,7 +5,7 @@ MutateCollection("folders.json", (folders) => {
 	logger.info("Updating Folders.");
 
 	for (const folder of folders) {
-		if(!folder.id?.startsWith("F")) {
+		if(folder.id === undefined || !folder.id.startsWith("F")) {
 			folder.id = CreateFolderID();
 			logger.info(`Updating ${folder.slug}`);
 		}
