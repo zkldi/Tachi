60c5b4b61e39a6d75b7f3342efe3e15ef0b8835c
beer-psi <92439990+beer-psi@users.noreply.github.com>
2026-05-23 20:06:55 +0700

    mark all game profiles as dirty instead of re-deriving

--- db/migrations/20260523071801_Colour_class_updates_for_maimai_DX_and_CHUNITHM.sql
+++ db/migrations/20260523071801_Colour_class_updates_for_maimai_DX_and_CHUNITHM.sql
@@ -1,26 +1,4 @@
-UPDATE game_profile 
-SET classes['colour'] = CASE
-    WHEN (ratings->>'naiveRate')::int >= 16750 THEN '"RAINBOW_EX_IV"'
-    WHEN (ratings->>'naiveRate')::int >= 16500 THEN '"RAINBOW_EX_III"'
-    WHEN (ratings->>'naiveRate')::int >= 16250 THEN '"RAINBOW_EX_II"'
-    WHEN (ratings->>'naiveRate')::int >= 16000 THEN '"RAINBOW_EX_I"'
-    WHEN (ratings->>'naiveRate')::int >= 15750 THEN '"RAINBOW_IV"'
-    WHEN (ratings->>'naiveRate')::int >= 15500 THEN '"RAINBOW_III"'
-    WHEN (ratings->>'naiveRate')::int >= 15250 THEN '"RAINBOW_II"'
-    WHEN (ratings->>'naiveRate')::int >= 15000 THEN '"RAINBOW"'
-    WHEN (ratings->>'naiveRate')::int >= 14750 THEN '"PLATINUM_II"'
-    WHEN (ratings->>'naiveRate')::int >= 14500 THEN '"PLATINUM"'
-    WHEN (ratings->>'naiveRate')::int >= 14250 THEN '"GOLD_II"'
-    WHEN (ratings->>'naiveRate')::int >= 14000 THEN '"GOLD"'
-    ELSE classes['colour']
-END
-WHERE game = 'maimaidx';
-
-UPDATE game_profile
-SET classes['colour'] = CASE
-    WHEN (ratings->>'naiveRating')::numeric >= 16.00 THEN classes['colour']
-    WHEN (ratings->>'naiveRating')::numeric >= 15.75 THEN '"PLATINUM_III"'
-    WHEN (ratings->>'naiveRating')::numeric >= 15.50 THEN '"PLATINUM_II"'
-    ELSE classes['colour']
-END
-WHERE game = 'chunithm';
+INSERT INTO game_profile_dirty (user_id, game)
+SELECT user_id, game
+FROM game_profile
+WHERE game IN ('maimaidx', 'chunithm');
